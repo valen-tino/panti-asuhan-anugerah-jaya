af45ff41237dc54781e3d7f8c34edd630ff5d345
enricojunior <zethsuboaragonchemistry@gmail.com>
2024-05-12 15:55:51 +0800

    improve the blogs

--- cta-script/cta-button.js
+++ cta-script/cta-button.js
@@ -1,1 +1,11 @@
-// Script for the CTA Button (Read More / See Less)
+const ReadMoreButton = document.querySelectorAll('.button.primary');
+
+ReadMoreButton.forEach(evokeBtn => {
+    evokeBtn.addEventListener('click', () => {
+        const secondSection = evokeBtn.closest('.card');
+        const viewMoreContents = secondSection.querySelector('.read-more-section');
+        
+        viewMoreContents.classList.toggle('read-more-section--display');
+        evokeBtn.textContent = viewMoreContents.classList.contains('read-more-section--display') ? 'Read Less' : 'Read More';
+    });
+});
